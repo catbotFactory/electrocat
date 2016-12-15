0704aa2d462e366d4c0b7a742ef6fcb961ed878d
gorhgorh <gorhgorh@lesnodebots.eu>
2016-12-16 00:22:46 +0100

    add deadeone to the pad

--- app/gamePad.js
+++ app/gamePad.js
@@ -10,15 +10,30 @@ function canGame () {
 }
 
 /**
+ * set a dead zone on given input to avoir gamepad jitter
+ *
+ * @param {int} number valute to consider
+ * @param {int} threshold of said angle
+ * @returns {int} cleaned axe value
+ */
+function deadZone (number, threshold) {
+  'use strict'
+  let percentage = (Math.abs(number) - threshold) / (1 - threshold)
+  if (percentage < 0) percentage = 0
+  return percentage * (number > 0 ? 1 : -1)
+}
+
+/**
  * pools the gamepad an return an object containing the 2 axes and the status of button A (pressed)
  *
- * @returns
+ * @returns {Object} pos object (x, y, a, timestamp (ts))
  */
 function poolGp () {
   const pad = navigator.getGamepads()[0] // only scan for the forst gp
+
   const padData = {
-    x: pad.axes[0],
-    y: pad.axes[1],
+    x: deadZone(pad.axes[0], 0.25),
+    y: deadZone(pad.axes[1], 0.25),
     a: pad.buttons[0].pressed,
     ts: pad.timestamp
   }
